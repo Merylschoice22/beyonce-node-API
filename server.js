2d049ad7000a522d96c046eabd5b82fb7aa83157
merylschoice22 <kekrieg.digital@gmail.com>
2021-06-02 23:47:20 +0200

    Added GET and POST

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+app.use(express.json());
 const albumsData = [
   {
     albumId: "10",
@@ -26,6 +26,36 @@ const albumsData = [
 
 app.get("/albums", (req, res) => {
   res.send(albumsData);
+});
+
+//Displaying an album object by entering the ID as the endpoint
+app.get("/albums/:albumID", (req, res) => {
+  //get ID from request path params
+  const albumID = req.params.albumID;
+  //get album with that ID - Look through the album array and find the album whose ID is the same as the requested ID number.
+  const album = albumsData.find((album) => {
+    return album.albumId == albumID;
+  });
+  //return album - If the album exist, then send it to the server. Otherwise make the status a 404, not found and send the status message to the server.
+  if (album) {
+    res.send(album);
+  } else {
+    res.status(404).send(404);
+  }
+});
+
+app.post("/albums", (req, res) => {
+  //get new album object from request - Enter information into the body of Postman. Since it is a json object, make sure to change the format from TEXT to JSON. In order to read the body here, add an app.use(express.json()) at the top of the page. The object in the body will not have an ID because we will not add a new item with our own ID - the system would assign the ID
+  const album = req.body;
+  console.log(req.body);
+  //Assign the ID by finding the ID number of the last item in the array and incrementing by one. Formatting is necessary to add a number to a Number and then to read it as a string since will be part of a json file
+  const albumId = (
+    parseInt(albumsData[albumsData.length - 1].albumId) + 1
+  ).toString();
+  //Add album object to array - First adding the ID that we created as the first part of the object, then the rest of the object will be filled with all of the content from inside the album object that we created (but not whole object itself!)
+  albumsData.push({ albumId, ...album });
+  console.log(albumsData); //So we can see here the final albums array
+  res.send({ success: true });
 });
 
 app.listen(5000, () => console.log("The server is listening to port 5000"));
